fb8c89b1c7e602a341b1e97d62c42a0fe1056441
sohjiro <felipe@makingdevs.com>
2014-02-28 01:04:06 -0600

    cambiando el destino de los hbs

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var handlebars = require('gulp-ember-handlebars');
+var concat = require('gulp-concat');
+var gulp = require('gulp');
+
+gulp.task('templates', function(){
+  gulp.src(['src/ratpack/public/scripts/templates/*.hbs'])
+    .pipe(handlebars({
+      outputType: 'browser'
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest('src/ratpack/public/scripts/templates/'));
+});
